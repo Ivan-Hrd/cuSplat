54cec81be65625c8e1e03897a580f53df865b00a
Ivan HUARD <ivcvhb@gmail.com>
2026-08-20 19:24:20 +0200

    fix(CMake): update RAPIDS fetch

--- cmake/thirdparty/get_raft.cmake
+++ cmake/thirdparty/get_raft.cmake
@@ -3,14 +3,15 @@ function(find_and_configure_raft)
     cmake_parse_arguments(PKG "" "${oneValueArgs}" "" ${ARGN})
 
     rapids_cpm_find(raft ${PKG_VERSION}
-        GLOBAL_TARGETS raft::raft
-        CPM_ARGS
-        GIT_REPOSITORY https://github.com/${PKG_FORK}/raft.git
-        GIT_TAG ${PKG_PINNED_TAG}
-        SOURCE_SUBDIR cpp
-        OPTIONS
+            GLOBAL_TARGETS raft::raft
+            CPM_ARGS
+            GIT_REPOSITORY https://github.com/${PKG_FORK}/raft.git
+            GIT_TAG ${PKG_PINNED_TAG}
+            SOURCE_SUBDIR cpp
+            EXCLUDE_FROM_ALL TRUE
+            OPTIONS
             "BUILD_TESTS OFF"
-            "BUILD_BENCH OFF"
+            "BUILD_PRIMS_BENCH OFF"
             "RAFT_COMPILE_LIBRARY OFF"
     )
 
@@ -23,9 +24,9 @@ endfunction()
 
 # Change pinned tag and fork here to test a commit in CI
 # To use a different RAFT locally, set the CMake variable
-# RPM_raft_SOURCE=/path/to/local/raft
-find_and_configure_raft(VERSION 24.08
-    FORK rapidsai
-    PINNED_TAG branch-24.08
-    CLONE_ON_PIN ON
+# CPM_raft_SOURCE=/path/to/local/raft
+find_and_configure_raft(VERSION 26.08.00
+        FORK rapidsai
+        PINNED_TAG v26.08.00
+        CLONE_ON_PIN ON
 )
--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: LicenseRef-NvidiaProprietary
 #
 # NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
@@ -7,9 +7,23 @@
 # disclosure or distribution of this material and related documentation
 # without an express license agreement from NVIDIA CORPORATION or
 # its affiliates is strictly prohibited.
-if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/TP_REDUCE_RAPIDS.cmake)
-  file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-24.08/RAPIDS.cmake
-      ${CMAKE_CURRENT_BINARY_DIR}/TP_REDUCE_RAPIDS.cmake
+
+set(CUSPLAT_RAPIDS_VERSION "26.08")
+set(CUSPLAT_RAPIDS_BRANCH "release/${CUSPLAT_RAPIDS_VERSION}")
+
+if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CUSPLAT_RAPIDS.cmake)
+  file(DOWNLOAD
+          https://raw.githubusercontent.com/rapidsai/rapids-cmake/${CUSPLAT_RAPIDS_BRANCH}/RAPIDS.cmake
+          ${CMAKE_CURRENT_BINARY_DIR}/CUSPLAT_RAPIDS.cmake
+          STATUS _dl_status
   )
+  list(GET _dl_status 0 _dl_code)
+  if(NOT _dl_code EQUAL 0)
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/CUSPLAT_RAPIDS.cmake)
+    message(FATAL_ERROR "Failed to download RAPIDS.cmake: ${_dl_status}")
+  endif()
 endif()
-include(${CMAKE_CURRENT_BINARY_DIR}/TP_REDUCE_RAPIDS.cmake)
+
+set(rapids-cmake-version "${CUSPLAT_RAPIDS_VERSION}")
+set(rapids-cmake-branch "${CUSPLAT_RAPIDS_BRANCH}")
+include(${CMAKE_CURRENT_BINARY_DIR}/CUSPLAT_RAPIDS.cmake)
