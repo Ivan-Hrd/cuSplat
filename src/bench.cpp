6532d37176296cde756baa011fbef30f798eedf2
Ivan HUARD <ivcvhb@gmail.com>
2026-08-26 10:05:35 +0200

    fix(bench.cu): give proper name the benchmark function

--- src/bench.cpp
+++ src/bench.cpp
@@ -13,7 +13,7 @@ void savePPM(const std::string& filename, float* image, int width, int height) {
         f << (int)(std::min(1.0f, image[i]) * 255) << " ";
 }
 
-static void mainBench(benchmark::State& state)
+static void rasterization_bench(benchmark::State& state)
 {
     std::cout << "Loading PLY file..." << std::endl;
     std::vector<Gaussian> gaussians = loadPLY("/home/h/CLionProjects/RT-Core-Gaussian-Splatting/point_cloud.ply");
@@ -35,6 +35,6 @@ static void mainBench(benchmark::State& state)
     savePPM("/home/h/Downloads/cuSplat/out/output.ppm", image, width, height);
     delete[] image;
 }
-BENCHMARK(mainBench)->Unit(benchmark::kSecond);
+BENCHMARK(rasterization_bench)->Unit(benchmark::kSecond);
 
 BENCHMARK_MAIN();
